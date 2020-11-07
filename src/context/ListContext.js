2cfde22d8b39c5ad7ebe29ff1b07c98c55b573c5
bunny98 <kavyansh.chourasia98@gmail.com>
2020-11-07 12:55:58 +0530

    UI Finished

--- src/context/ListContext.js
+++ src/context/ListContext.js
@@ -5,11 +5,29 @@ const ListContext = React.createContext();
 export const ListProvider = ({ children }) => {
     const [listItems, setListItems] = useState([]);
 
+    const imageSrcProvider = (num) => {
+        switch (num) {
+            case 1:
+                return require('../../assets/images/1.jpg');
+            case 2:
+                return require('../../assets/images/2.jpg');
+            case 3:
+                return require('../../assets/images/3.jpg');
+            case 4:
+                return require('../../assets/images/4.jpg');
+            case 5:
+                return require('../../assets/images/5.jpg');
+            default:
+                return require('../../assets/images/1.jpg');
+        }
+    }
+
     const addListItem = () => {
         const toAddItems = [];
         for (let i = 0; i < 10; ++i) {
             // console.log(`LIST ITEM ${listItems.length + i}`);
-            toAddItems.push({ title: `LIST ITEM ${listItems.length + i}` });
+            const imageSrc = imageSrcProvider(Math.floor(Math.random() * 6) + 1);
+            toAddItems.push({ title: `LIST ITEM ${listItems.length + i}`, imageSrc });
         }
         setListItems([...listItems, ...toAddItems]);
     }
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,27 +1,59 @@
-import React, { useContext } from 'react';
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import React, { useContext, useEffect } from 'react';
+import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
 
 import ListContext from '../context/ListContext'
 import { SafeAreaView } from 'react-navigation';
 
 const HomeScreen = () => {
     const { data, addListItem } = useContext(ListContext);
-    console.log(data.length);
+    // console.log(data.length);
+    useEffect(() => addListItem(), []);
     return <View style={{ flex: 1 }}>
-        <Button onPress={addListItem} title="Add item" />
         <SafeAreaView>
             <FlatList
+                showsVerticalScrollIndicator={false}
+                onEndReached={addListItem}
+                onEndReachedThreshold={0.8}
                 data={data}
                 keyExtractor={item => item.title}
-                renderItem={({ item }) => <Text>
-                    {item.title}
-                </Text>
+                renderItem={({ item }) => {
+                    return <View>
+                        <Image
+                            style={styles.image}
+                            source={item.imageSrc} />
+                        <View style={styles.horizontalView}>
+                            <Text style={styles.boldText}>
+                                {item.title}
+                            </Text>
+                            <Text>
+                                Constant Text
+                            </Text>
+                        </View>
+                    </View>
+                }
                 }
             />
         </SafeAreaView>
     </View>
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    image: {
+        height: 200,
+        width: 340,
+        margin: 10,
+        borderRadius: 10,
+    },
+    horizontalView: {
+        flexDirection: "row",
+        alignItems: 'flex-end',
+        justifyContent: 'space-between',
+        paddingHorizontal: 10,
+        paddingBottom: 5,
+    },
+    boldText: {
+        fontWeight: 'bold',
+    }
+});
 
 export default HomeScreen;
